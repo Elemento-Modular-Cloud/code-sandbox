139c852a5458f619c43c90e902e496005b931dd3
Gabriele Fronzé <sucre.91@hotmail.it>
2024-01-14 00:53:12 +0100

    WIP

--- git-software-updater/git_software_updater.py
+++ git-software-updater/git_software_updater.py
@@ -7,7 +7,8 @@ from shutil import rmtree
 def get_target_path(target_path, repo_url):
     # Extract repository name from the URL
     repo_name = repo_url.split('/')[-1].replace('.git', '')
-    
+    target_path = os.path.expanduser(target_path)
+
     # If target_path ends with the repo_name, use it directly
     if target_path.endswith(repo_name):
         return target_path
@@ -21,8 +22,11 @@ def get_target_path(target_path, repo_url):
 
 def clone_repo(repo_url, target_path, branch, ssh_key_path):
     if repo_url.startswith('https://'):
+        print("HTTPS")
+        print(target_path)
         return Repo.clone_from(repo_url, target_path, branch=branch)
     elif repo_url.startswith('git@'):
+        print("GIT")
         if ssh_key_path:
             return Repo.clone_from(repo_url, target_path, env={"GIT_SSH_COMMAND": f"ssh -i {ssh_key_path}"})
         else:
@@ -53,77 +57,71 @@ def clone_or_update_repo(repo_url,
                          with_submodules=False,
                          ssh_key_path=None,
                          force_reset=False):
-    final_target_path = os.path.abspath(get_target_path(target_path, repo_url))
+    final_target_path = get_target_path(target_path, repo_url)
 
-    try:
-        print(f"Starting cloning to {final_target_path}")
-        # Check if the directory is a valid Git repository
-        if os.path.exists(final_target_path):
-            print("Path exists")
+    print(f"Starting cloning to {final_target_path}")
+    # Check if the directory is a valid Git repository
+    if os.path.exists(final_target_path):
+        print("Path exists")
+        try:
+            repo = Repo(final_target_path)
+            print("Repo found")
+        except Exception:
+            print("Path is not empty, but no git repo is found")
+            if force_reset:
+                print("Resetting path to allow cloning")
+                # rmtree(final_target_path)
+                repo = clone_repo(repo_url, final_target_path, branch, ssh_key_path)
+                print("Repo cloned")
+            else:
+                repo = force_git_recover(final_target_path=final_target_path,
+                                            repo_url=repo_url,
+                                            branch=branch)
+        if not repo.bare:
             try:
-                repo = Repo(final_target_path)
-                print("Repo found")
+                print("Pulling")
+                repo.remotes.origin.pull()
+                print("Repo pulled")
             except Exception:
-                print("Path is not empty, but no git repo is found")
-                if force_reset:
-                    print("Resetting path to allow cloning")
-                    rmtree(final_target_path)
-                    repo = clone_repo(repo_url, final_target_path, branch, ssh_key_path)
-                    print("Repo cloned")
-                else:
-                    print("Reinitialising repo")
-                    repo = force_git_recover(final_target_path=final_target_path,
-                                             repo_url=repo_url,
-                                             branch=branch)
-                    print("Reinit done")
-            if not repo.bare:
-                try:
-                    print("Pulling")
-                    repo.remotes.origin.pull()
-                    print("Repo pulled")
-                except Exception:
-                    print("Reinitialising repo")
-                    repo = force_git_recover(final_target_path=final_target_path,
-                                             repo_url=repo_url,
-                                             branch=branch)
-                    print("Reinit done")
-            else:
-                print(f"Empty or invalid repo path at {final_target_path}")
-                # If the directory exists but is not a valid Git repository
-                raise InvalidGitRepositoryError(
-                    f"'{final_target_path}' is not a valid Git repository.")
+                print("Reinitialising repo")
+                repo = force_git_recover(final_target_path=final_target_path,
+                                            repo_url=repo_url,
+                                            branch=branch)
+                print("Reinit done")
         else:
-            print("Repo doesn't exist")
-            # Clone repository based on URL and optional SSH key
-            repo = clone_repo(repo_url, final_target_path, branch, ssh_key_path)
-            print("Repo cloned")
-
-        if force_reset:
-            if branch:
-                print(f"Resetting repo and checking out {branch}")
-                repo.git.reset('--hard', f'origin/{branch}')
-            else:
-                print("resetting repo")
-                repo.git.reset('--hard')
-        elif branch:
-            print(f"Checking out branch {branch}")
-            repo.git.checkout(branch)
-        if commit:
-            print(f"Checking out commit {commit}")
-            repo.git.checkout(commit)
-
-        if with_submodules:
-            print(f"Initializing all submodules")
-            repo.git.submodule('update', '--init', '--recursive')
-
-        print(
-            f"Successfully processed {repo_url} with branch {branch} and commit {commit} at {final_target_path}")
-    except InvalidGitRepositoryError:
-        print(
-            f"'{final_target_path}' is not a valid Git repository. Cloning from '{repo_url}'.")
+            print(f"Empty or invalid repo path at {final_target_path}")
+            # If the directory exists but is not a valid Git repository
+            raise InvalidGitRepositoryError(
+                f"'{final_target_path}' is not a valid Git repository.")
+    else:
+        print("Repo doesn't exist")
+        os.mkdir(final_target_path)
+        os.chdir(final_target_path)
+        # Clone repository based on URL and optional SSH key
         repo = clone_repo(repo_url, final_target_path, branch, ssh_key_path)
-    except Exception as e:
-        print(f"Failed to process {repo_url} at {final_target_path}. Error: {e}")
+        print("Repo cloned")
+
+    if force_reset:
+        if branch:
+            print(f"Resetting repo and checking out {branch}")
+            repo.git.reset('--hard', f'origin/{branch}')
+        else:
+            print("resetting repo")
+            repo.git.reset('--hard')
+    elif branch:
+        print(f"Checking out branch {branch}")
+        repo.git.checkout(branch)
+    
+    if commit:
+        print(f"Checking out commit {commit}")
+        repo.git.checkout(commit)
+
+    if with_submodules:
+        print(f"Initializing all submodules")
+        repo.git.submodule('update', '--init', '--recursive')
+
+    print(
+        f"Successfully processed {repo_url} with branch {branch} and commit {commit} at {final_target_path}")
 
 
 def find_config_file():
